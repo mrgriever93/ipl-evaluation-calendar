11167d0c5af0b9e3d9472dc95bd074b851c5dd56
Miguel Cerejo <migas_cerejo-1993@hotmail.com>
2022-03-31 04:00:43 +0100

    create component for empty table or no results

---
+++ resources/components/EmptyTable/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {Button, Header, Icon, Table} from "semantic-ui-react";
+import ShowComponentIfAuthorized from "../ShowComponentIfAuthorized";
+import {useTranslation} from "react-i18next";
+
+const EmptyTable = ({onBtnClick, colspan, label, permissions, isLoading = false}) => {
+    const { t } = useTranslation();
+    return (
+        <Table.Row key="empty-table">
+            <Table.Cell colSpan={colspan} textAlign="center" className={"padding-xl empty-table-row"}>
+                {
+                    isLoading ?
+                    (
+                        <div className={"margin-y-l"}>{t("A carregar registos...")}</div>
+                    ) : (
+                        <Wrapper>
+                            <Header as='h2' color='red' className={"margin-y-l"} disabled>
+                                {label}
+                            </Header>
+                            <div className={"margin-bottom-xl"}>
+                                <Icon.Group size='huge'>
+                                    <Icon size='large' color='grey' name='clipboard outline' className={"margin-none"}/>
+                                    <Icon size='tiny' color='grey' name='cogs' />
+                                </Icon.Group>
+                            </div>
+                            <div>
+                                <ShowComponentIfAuthorized permission={permissions}>
+                                    <Button color="green" content={t("Novo")} icon='plus' labelPosition='right' onClick={onBtnClick}/>
+                                </ShowComponentIfAuthorized>
+                            </div>
+                        </Wrapper>
+                    )
+                }
+            </Table.Cell>
+        </Table.Row>
+    )
+}
+
+export default EmptyTable;
