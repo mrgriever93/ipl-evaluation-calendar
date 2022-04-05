d14231647410eddc18ae17042951b3bed46148d3
Miguel Cerejo <migas_cerejo-1993@hotmail.com>
2022-04-05 01:14:55 +0100

    Update AcademicYear list Resource

--- app/Http/Resources/AcademicYearResource.php
+++ app/Http/Resources/AcademicYearResource.php
@@ -9,11 +9,13 @@ class AcademicYearResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id'    => $this->id,
-            'code'  => $this->code,
-            'selected' => $this->selected,//$request->hasCookie('academic_year') ? $this->id == $request->cookie('academic_year') : $this->active,
-            'display' => $this->display,
-            'active' => $this->active,
+            'id'                => $this->id,
+            'code'              => $this->code,
+            'display'           => $this->display,
+            'selected'          => !!$this->selected,//$request->hasCookie('academic_year') ? $this->id == $request->cookie('academic_year') : $this->active,
+            'active'            => !!$this->active,
+            'isActiveLoading'   => false,
+            'isSelectedLoading' => false
         ];
     }
 }
