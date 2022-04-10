1528e54c17fcf448ad525fa64a5eb1a5543fef6d
Miguel Cerejo <migas_cerejo-1993@hotmail.com>
2022-04-10 15:11:55 +0100

    Add Dropdown filters to easier maintance

---
+++ resources/components/Filters/Degree.js
@@ -0,0 +1,34 @@
+import React, {useEffect, useState} from 'react';
+import {Form} from 'semantic-ui-react';
+import axios from 'axios';
+import {useTranslation} from "react-i18next";
+
+const FilterOptionDegree = ({widthSize, eventHandler}) => {
+    const { t } = useTranslation();
+    const [loading, setLoading] = useState(false);
+    const [degreeOptions, setDegreeOptions] = useState([]);
+    const [degree, setDegree] = useState();
+
+    useEffect(() => {
+        setLoading(true);
+        axios.get('/courses/degrees').then((response) => {
+            if (response.status >= 200 && response.status < 300) {
+                response.data.unshift({value:'', text: "All Degrees"},);
+                setDegreeOptions(response.data);
+                setLoading(false);
+            }
+        });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    const filterByDegree = (e, {value}) => {
+        setDegree(value);
+        eventHandler(value);
+    };
+
+    return (
+        <Form.Dropdown width={widthSize} selection value={degree} options={degreeOptions} label={t("Grau de ensino")} placeholder={t("Grau de ensino")} loading={loading} onChange={filterByDegree}/>
+    );
+};
+
+export default FilterOptionDegree;
