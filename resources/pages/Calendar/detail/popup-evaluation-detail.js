f5d1bedcc74f50e3dc8de67eb711fa973a300e40
Alexandre Santos <alexandre.santos@mediaweb.pt>
2022-05-30 00:56:20 +0100

    update comments section and blank slate when no comments exist

--- resources/pages/Calendar/detail/popup-evaluation-detail.js
+++ resources/pages/Calendar/detail/popup-evaluation-detail.js
@@ -5,7 +5,7 @@ import React, {useEffect, useState} from 'react';
 // import {useParams, useNavigate} from "react-router-dom";
 import {useTranslation} from "react-i18next";
 // import {Field, Form as FinalForm} from 'react-final-form';
-import { Button, Form, Header, Icon, List, Modal, Comment, Divider } from 'semantic-ui-react';
+import { Button, Form, Header, Icon, List, Modal, Comment, Divider, Segment } from 'semantic-ui-react';
 import {toast} from 'react-toastify';
 // import Swal from 'sweetalert2';
 // import withReactContent from 'sweetalert2-react-content';
@@ -60,21 +60,43 @@ const PopupEvaluationDetail = ( {isOpen, onClose, examId} ) => {
         }).then((res) => {
             if (res.status === 201) {
                 setCommentText('');
-                toast(t('calendar.O comentário foi adicionado com sucesso!'), successConfig);
+                toast(t('O comentário foi adicionado com sucesso!'), successConfig);
                 setCommentsList(res.data);
             } else {
-                toast(t('calendar.Ocorreu um erro ao adicionar o comentário!'), errorConfig);
+                toast(t('Ocorreu um erro ao adicionar o comentário!'), errorConfig);
             }
         });
     };
 
-    const ignoreComment = (commentId) => {
-        axios.post(`/comment/${commentId}/ignore`).then((res) => {
+    const hideCommentHandler = (commentId) => {
+        axios.post(`/comment/${commentId}/hide`).then((res) => {
             if (res.status === 200) {
-                toast(t('calendar.Comentário ignorado com sucesso!'), successConfig);
+                toast(t('Comentário escondido com sucesso!'), successConfig);
                 setCommentsList(res.data);
             } else {
-                toast(t('calendar.Ocorreu um erro ao ignorar o comentário!'), successConfig);
+                toast(t('Ocorreu um erro ao esconder o comentário!'), errorConfig);
+            }
+        });
+    };
+
+    const showCommentHandler = (commentId) => {
+        axios.post(`/comment/${commentId}/show`).then((res) => {
+            if (res.status === 200) {
+                toast(t('Comentário mostrado com sucesso!'), successConfig);
+                setCommentsList(res.data);
+            } else {
+                toast(t('Ocorreu um erro ao mostrar o comentário!'), errorConfig);
+            }
+        });
+    };
+
+    const deleteCommentHandler = (commentId) => {
+        axios.delete(`/comment/${commentId}`).then((res) => {
+            if (res.status === 200) {
+                toast(t('Comentário eliminado com sucesso!'), successConfig);
+                setCommentsList(res.data);
+            } else {
+                toast(t('Ocorreu um erro ao eliminar o comentário!'), errorConfig);
             }
         });
     };
@@ -139,21 +161,37 @@ const PopupEvaluationDetail = ( {isOpen, onClose, examId} ) => {
                         <ShowComponentIfAuthorized permission={[SCOPES.VIEW_COMMENTS, SCOPES.ADD_COMMENTS]}>
                             <Comment.Group>
                                 <ShowComponentIfAuthorized permission={[SCOPES.VIEW_COMMENTS]}>
-                                    <Header as="h3" dividing>Comentários</Header>
+                                    <div className='exam-detail-content-header'>
+                                        <div className='exam-detail-content-header-title'>
+                                            <Header as="h3">Comentários</Header>
+                                        </div>
+                                        <div className='exam-detail-content-header-actions'>
+                                            {!isPublished && (
+                                                <ShowComponentIfAuthorized permission={[SCOPES.ADD_COMMENTS]}>
+                                                    <Button 
+                                                        content={ t('Adicionar comentário')}
+                                                        labelPosition="right" 
+                                                        icon="send" 
+                                                        primary onClick={() => addComment(examDetailObject?.id)} 
+                                                    />
+                                                </ShowComponentIfAuthorized>
+                                            )}
+                                        </div>
+                                    </div>
                                     {!isPublished && (
                                         <ShowComponentIfAuthorized permission={[SCOPES.ADD_COMMENTS]}>
                                             <Form reply className='focus--expander'>
                                                 <Form.TextArea rows={2} placeholder={ t('Adiciona aqui o teu comentário') } value={commentText} onChange={(ev, {value}) => setCommentText(value)}/>
-                                                <Button style={{marginBottom: 'var(--space-base)' }} onClick={() => addComment(examDetailObject?.id)} content="Adicionar comentário" labelPosition="right" icon="send" floated='right' primary />
+                                                {/* <Button style={{marginBottom: 'var(--space-base)' }} onClick={() => addComment(examDetailObject?.id)} content="Adicionar comentário" labelPosition="right" icon="send" floated='right' primary /> */}
                                             </Form>
                                             <Divider clearing />
                                         </ShowComponentIfAuthorized>
                                     )}
                                     {commentsList?.filter((x) => (showIgnoredComments ? true : !x.ignored))?.map((comment, commentIndex) => (
-                                        <Comment key={commentIndex}>
+                                        <Comment key={commentIndex} className={comment.ignored ? 'comment--ignored' : ''}>
                                             <Comment.Avatar src={`https://avatars.dicebear.com/api/initials/${comment.user.initials}.svg?w=50&h=50&mood[]=sad&mood[]=happy`}/>
-                                            <Comment.Content style={comment.ignored ? {backgroundColor: 'lightgrey'} : {}}>
-                                                <Comment.Author as="a">{comment.user.name}</Comment.Author>
+                                            <Comment.Content>
+                                                <Comment.Author as="span">{comment.user.name}</Comment.Author>
                                                 <Comment.Metadata>
                                                     <div>{comment.date_label}</div>
                                                 </Comment.Metadata>
@@ -167,15 +205,42 @@ const PopupEvaluationDetail = ( {isOpen, onClose, examId} ) => {
                                                 </Comment.Text>
                                                 <Comment.Actions>
                                                     {!comment.ignored && (
-                                                        <Comment.Action onClick={() => ignoreComment(comment.id)}>
-                                                            Ignorar
+                                                        <Comment.Action onClick={() => hideCommentHandler(comment.id)}>
+                                                            { t('Esconder') }
                                                         </Comment.Action>
+                                                    )}
+                                                    { !!comment.ignored && (
+                                                        <Comment.Action onClick={() => showCommentHandler(comment.id)}>
+                                                            { t('Mostrar') }
+                                                        </Comment.Action>
+                                                    )}
+                                                    { moment(new Date()).diff(moment(comment.date), 'minutes') <= 15 && (
+                                                        <>
+                                                            <Comment.Action onClick={() => deleteCommentHandler(comment.id)} style={{color: 'red'}}>
+                                                                { t('Remover') }
+                                                            </Comment.Action>
+                                                        </>
                                                     )}
                                                 </Comment.Actions>
                                             </Comment.Content>
                                         </Comment>
                                     ))}
-                                    { commentsList?.length == 0 && (<div>No comments</div>)}
+                                    { (commentsList?.length == 0 || (!showIgnoredComments && (
+                                                (commentsList?.filter((x) => (x.ignored)).length > 0 && commentsList?.filter((x) => (!x.ignored)).length == 0))) ) && 
+                                    (
+                                        <Segment placeholder textAlign="center">
+                                            <Header icon>
+                                                <Icon name='comments outline' />
+                                                <div>{ t('Não existem comentários para esta avaliação...')}</div>
+                                            </Header>
+                                            { commentsList?.filter((x) => (x.ignored)).length > 1 && (
+                                                <div> { t('Existem') +" " + commentsList?.filter((x) => (x.ignored)).length + " " + t('comentários escondidos') }</div>
+                                            ) }
+                                            { commentsList?.filter((x) => (x.ignored)).length == 1 && (
+                                                <div> { t('Existe') +" " + commentsList?.filter((x) => (x.ignored)).length + " " + t('comentário escondido') }</div>
+                                            ) }                                               
+                                        </Segment>
+                                    )}
                                 </ShowComponentIfAuthorized>
                             </Comment.Group>
                         </ShowComponentIfAuthorized>
