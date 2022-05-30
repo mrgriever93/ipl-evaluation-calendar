951c4990c03830d9932862b2fcbc2b55342395ef
Miguel Cerejo <migas_cerejo-1993@hotmail.com>
2022-05-30 01:46:06 +0100

    add methods flag to the ones done

--- app/Http/Resources/AvailableMethodsResource.php
+++ app/Http/Resources/AvailableMethodsResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Exam;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class AvailableMethodsResource extends JsonResource
@@ -13,7 +14,7 @@ class AvailableMethodsResource extends JsonResource
             'name'      => ($request->header("lang") == "en" ? $this["evaluation_type"]["name_en"] : $this["evaluation_type"]["name_pt"]),
             'minimum'   => $this["minimum"],
             'weight'    => $this["weight"],
-            'is_done'   => false,
+            'is_done'   => Exam::where("method_id", $this["id"])->exists()
         ];
     }
 }
